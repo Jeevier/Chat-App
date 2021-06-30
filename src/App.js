f44428b5ba0ada980a18d35f03560ad23747b238
SanyamJain <sanyamjain10168@gmail.com>
2021-06-30 21:23:39 +0530

    videocall feature

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { ChatEngine } from 'react-chat-engine';
 
 import LoginForm from './components/LoginForm';
 import './App.css';
-
+import ChatSetter from './components/ChatSetter';
 const projectID = '18c46a88-9ac4-4f14-87c7-f5771451ae80';
 
 const App = () => {
@@ -14,6 +14,7 @@ const App = () => {
       projectID={projectID}
       userName={localStorage.getItem('username')}
       userSecret={localStorage.getItem('password')}
+      renderNewMessageForm={(creds, chatID) => <ChatSetter/>}
       onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
---
+++ src/components/ChatSetter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NewMessageForm} from 'react-chat-engine';
+const ChatSetter = () => {
+    return (
+      <>
+  <NewMessageForm></NewMessageForm>
+  <form action="https://viochat.tech/" method="get"  target="_blank"><button className="button">Video Call</button></form>
+   </>
+        
+    )
+}
+
+export default ChatSetter;
