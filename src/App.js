c1c688d1291a7682600937404970dba396ef8e5d
SanyamJain <sanyamjain10168@gmail.com>
2021-07-06 15:16:31 +0530

    logout feature added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { ChatEngine } from 'react-chat-engine';
+import { ChatEngine } from "react-chat-engine";
 
-import LoginForm from './components/LoginForm';
-import './App.css';
-import ChatSetter from './components/ChatSetter';
-const projectID = '18c46a88-9ac4-4f14-87c7-f5771451ae80';
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+import ChatSetter from "./components/ChatSetter";
+const projectID = "18c46a88-9ac4-4f14-87c7-f5771451ae80";
 
 const App = () => {
-  if (!localStorage.getItem('username')) return <LoginForm />;
+  if (!localStorage.getItem("username")) return <LoginForm />;
 
   return (
     <ChatEngine
       height="89vh"
       projectID={projectID}
-      userName={localStorage.getItem('username')}
-      userSecret={localStorage.getItem('password')}
-      renderNewMessageForm={(creds, chatID) => <ChatSetter/>}
-      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderNewMessageForm={(creds, chatID) => <ChatSetter />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
     />
   );
 };
--- src/components/ChatSetter.jsx
+++ src/components/ChatSetter.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { NewMessageForm } from "react-chat-engine";
-import {PlayCircleOutlined} from '@ant-design/icons';
+import { PlayCircleOutlined, LogoutOutlined } from "@ant-design/icons";
 const ChatSetter = () => {
+  const logoutHandler = () => {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+    document.location.reload();
+  };
   return (
     <>
-      <NewMessageForm></NewMessageForm>
-      <form  className="form__video-button" action="https://viochat.tech/" method="get" target="_blank">
-        <button className="button-video"><PlayCircleOutlined /> Video Call</button>
+      <NewMessageForm />
+      <form
+        className="form__video-button"
+        action="https://viochat.tech/"
+        method="get"
+        target="_blank"
+      >
+        <LogoutOutlined
+          style={{ color: "red", fontSize: "1.3rem" }}
+          onClick={logoutHandler}
+        />
+        <button className="button-video">
+          <PlayCircleOutlined /> Video Call
+        </button>
       </form>
     </>
   );
